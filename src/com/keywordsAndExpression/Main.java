e2c47fdca3f6f06f24128e5503e8653b0f3632a2
Byas <byas.dhungana@sjsu.edu>
2020-10-05 22:15:40 -0700

    initial commit: Udemy Advance Java Practice

---
+++ src/com/keywordsAndExpression/Main.java
@@ -0,0 +1,13 @@
+package com.keywordsAndExpression;
+
+//main method to run the code
+public class Main {
+    public static void main(String[] args) {
+        int myVariable = 50;
+        myVariable++;
+        // myVariable--;
+        System.out.println("This is a test " + myVariable);
+
+    }
+}
+
