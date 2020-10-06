8bfdad36fb029730e7ba169db6b994096d575ba8
Byas <byas.dhungana@sjsu.edu>
2020-10-05 22:31:41 -0700

    Modified Print message Removed an extra condition from if else statements

--- src/com/codeBlock/Challange.java
+++ src/com/codeBlock/Challange.java
@@ -25,8 +25,6 @@ public class Challange {
 
         if (playerScore > 1000) {
             return 1;
-        } else if (playerScore > 500 && playerScore < 1000) {
-            return 2;
         } else if (playerScore > 100 && playerScore < 500) {
             return 3;
         } else {
